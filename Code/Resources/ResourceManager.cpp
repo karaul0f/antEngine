f374d3af2fef83298f8de5e700cb91abb41da8c5
antour <andbobngs.ru@gmail.com>
2021-08-03 21:52:54 +0700

    Загрузка ресурсов при запуске игры

--- Code/Resources/ResourceManager.cpp
+++ Code/Resources/ResourceManager.cpp
@@ -1,6 +1,14 @@
 #include "ResourceManager.h"
 #include "../Core/Logger.h"
 #include "tinyxml2.h"
+#include "tinyxml2.cpp"
+
+namespace
+{
+	const std::string SPRITE_TYPE = "Sprite";
+	const std::string MUSIC_TYPE = "Music";
+	const std::string SOUND_TYPE = "Sound";
+}
 
 namespace Resources
 {
@@ -8,12 +16,60 @@ namespace Resources
 void ResourceManager::Init()
 {
 	INFO("Инициализация менеджера ресурсов");
-
+	ResourceLoad();
+	
 }
 //-----------------------------------------------------------------
 void ResourceManager::Deinit()
 {
 	
+}
+void ResourceManager::ResourceLoad()
+{
+	tinyxml2::XMLDocument doc;
+	doc.LoadFile("Data/GameConfig.xml");
+
+	tinyxml2::XMLElement* gameConfigElement = doc.FirstChildElement("GameConfig");
+	if (gameConfigElement != nullptr) 
+	{
+		tinyxml2::XMLElement* resources = gameConfigElement->FirstChildElement("Resources");
+
+		for (tinyxml2::XMLElement* resource = resources->FirstChildElement("Resource");
+			resource != nullptr; resource = resource->NextSiblingElement())
+		{
+			if (SPRITE_TYPE == resource->Attribute("Type")) //SPRITE_TYPE.compare(resource->Attribute("Type"))
+			{
+				m_textures[resource->Attribute("Name")].loadFromFile(CreatePath(resource->Attribute("Path")));
+				m_sprites[resource->Attribute("Name")].setTexture(m_textures[resource->Attribute("Name")]);
+			}
+			else if (MUSIC_TYPE == resource->Attribute("Type"))
+			{
+				m_musics[resource->Attribute("Name")].openFromFile(CreatePath(resource->Attribute("Path")));
+			}
+			else if (SOUND_TYPE == resource->Attribute("Type"))
+			{
+				m_soundBuffers[resource->Attribute("Name")].loadFromFile(CreatePath(resource->Attribute("Path")));
+				m_sounds[resource->Attribute("Name")].setBuffer(m_soundBuffers[resource->Attribute("Name")]);
+			}
+		}
+	}
+}
+std::string ResourceManager::CreatePath(std::string path)
+{
+	path = "Data/Resources/" + path;
+	return path;
+}
+sf::Sprite& ResourceManager::GetSpriteByName(std::string name)
+{
+	return m_sprites[name];
+}
+sf::Music& ResourceManager::GetMusicByName(std::string name)
+{
+	return m_musics[name];
+}
+sf::Sound& ResourceManager::GetSoundByName(std::string name)
+{
+	return m_sounds[name];
 }
 //-----------------------------------------------------------------
 }
--- Code/Resources/ResourceManager.h
+++ Code/Resources/ResourceManager.h
@@ -3,6 +3,11 @@
 #include <unordered_map>
 
 #include "LevelLoader.h"
+#include <map>
+#include "SFML/Graphics.hpp"
+#include <SFML/Audio/Music.hpp>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
 
 namespace Resources
 {
@@ -13,6 +18,13 @@ namespace Resources
 		ResourceManager() {}
 		ResourceManager(const ResourceManager& instance) = delete;
 		ResourceManager& operator=(const ResourceManager&) = delete;
+
+		std::map<std::string, sf::Texture>		m_textures;
+		std::map<std::string, sf::Sprite>		m_sprites;
+		std::map<std::string, sf::Music>		m_musics;
+		
+		std::map<std::string, sf::SoundBuffer>	m_soundBuffers;
+		std::map<std::string, sf::Sound>		m_sounds;
 	public:
 		static ResourceManager& Instance()
 		{
@@ -25,5 +37,20 @@ namespace Resources
 
 		// Уничтожаем все ресурсы
 		void Deinit();
+
+		// Загружаем ресурсы для игры
+		void ResourceLoad();
+
+		// Создаём путь к файлу 
+		std::string CreatePath(std::string path);
+
+		// Получаем спрайт по имени
+		sf::Sprite& GetSpriteByName(std::string name);
+
+		// Получаем музыку по имени
+		sf::Music& GetMusicByName(std::string name);
+
+		// Получаем звук по имени
+		sf::Sound& GetSoundByName(std::string name);
 	};
 }
